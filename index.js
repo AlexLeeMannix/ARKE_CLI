bf97d0ec4ed11ea7ad06401ee1ae902769d871f0
Alex Mannix <alexleemannix@gmail.com>
2018-08-27 23:51:00 -0400

    with callbacks from chris

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node --harmony
 
-console.log('Hello World')
+
+
+
 
 const fs = require('fs')
 const path = require('path')
 const program = require('commander');
 const co = require('co');
 const prompt = require('co-prompt');
+const useAccessObject = require('./main.js');
+
+console.log('running indexJS')
+
 
 
 program
@@ -14,15 +20,19 @@ program
   .action(() => {
     console.log('action is being run')
     co(function *(){
-      let fileObject = {}
       console.log('cofunction is being run')
-      let idAccessKey = yield prompt('Access Key ID: ');
-      let accessKey = yield prompt.password('Access Key: ')
-      const accessObject = JSON.stringify({ACCESS_KEY_ID: idAccessKey, ACCESS_KEY: accessKey})
-      fs.writeFile('AWS_access.js', accessObject, (err) => {
-        if (err) throw err;
-        else console.log('file created!')
-      })
+      let idAccessKey = yield prompt('AWS Access Key ID: ');
+      let accessKey = yield prompt.password('AWS Access Key: ')
+      console.log(idAccessKey)
+      console.log(accessKey)
+      useAccessObject({ACCESS_KEY_ID: idAccessKey, ACCESS_KEY: accessKey})
+
     })
   })
   .parse(process.argv)
+
+
+
+
+
+//somehow export the idaccesskey and accessKey directly to main js - need to access it outside the scope
--- main.js
+++ main.js
@@ -1,29 +1,54 @@
 #!/usr/bin/env node --harmony
 
 const fs = require('fs')
-const processFile = () =>{
-  return JSON.parse(data);
+
+
+
+
+
+
+console.log('RUNNING MAIN.JS')
+//console.log(accessObject)
+
+
+function useAccessObject (accessObject) {
+
+  console.log(accessObject)
 }
 
-const readContent = (callback) => {
-  fs.readFile('./AWS_access.js', (err, data) => {
-    if (err) throw err;
-    else{
-      console.log('running read file')
-      console.log('data',data)
-      let accessParced = JSON.parse(data);
-      callback(null, accessParced)
-    }
-  });
-}
 
-readContent((err, readFileData) => {
-  console.log(readFileData)
-  return readFileData
-})
+// const processFile = () =>{
+//   return JSON.parse(data);
+// }
+// let theDataWeWant;
+// const readContent = (callback) => {
+//   fs.readFile('./AWS_access.js', (err, data) => {
+//     if (err) throw err;
+//     else{
+//       console.log('running read file')
+//       console.log('data',data)
+//       let accessParced = JSON.parse(data);
+//       console.log('about to run callback')
+//       theDataWeWant = callback(null, accessParced)
+//       console.log('callback is done')
+//       return theDataWeWant
+//     }
+//   })
+//
+//
+// }
+//
+// readContent((err, readFileData) => {
+//   console.log('readFileData',readFileData)
+//   return readFileData
+// })
 
+
+//console.log('authInfo',authInfo)
 //can i handle it with a promise? need to access the data outside of the function (i dont want to wrap all of our code in one functuon....i dont think that would even work)
 
 
-// console.log('key id',authInfo.ACCESS_KEY_ID);
-// console.log('key',authInfo.ACCESS_KEY);
+//console.log('key id',authInfo.ACCESS_KEY_ID);
+//console.log('key',authInfo.ACCESS_KEY);
+
+module.exports = useAccessObject;
